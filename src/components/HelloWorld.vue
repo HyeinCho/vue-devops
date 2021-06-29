5fdda1fdb586291abab61110a08b098dbaa0eb5c
HyeinCho <chohyein96@gmail.com>
2021-06-29 14:05:53 +0900

    real final test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
-    <!-- <h1>{{ msg }}</h1> -->
-    <h2>헣헣.. 실수</h2>
+    <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
